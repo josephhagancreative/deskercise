eff95a6b6457ac54db26bad98030dd41d6c98e86
Joe Hagan <jhaganfilm@gmail.com>
2022-08-17 21:55:27 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { NextPage } from "next"
+
+const IndexPage: NextPage = () => {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-xl text-center p-2 mt-2 shadow-md">Deskercise</h1>
+    </div>
+  )
+}
+
+export default IndexPage
